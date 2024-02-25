3c2641cc923efa51c7e978742efdd6b36392b009
Srisankar2001 <srisank-se20028@stu.kln.ac.lk>
2024-02-25 12:32:43 +0530

    cart

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Mobile from "./pages/Mobile";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 import ViewProductPage from "./pages/ViewProductPage";
+import CartPage from "./pages/CartPage";
 function App() {
   return (
     <div className="App">
@@ -21,7 +22,7 @@ function App() {
           <Route path="/home" element={<HomePage/>}/>
           <Route path="/products" element={<ProductPage/>}/>
           <Route path="/products/:mobileId" element={<ViewProductPage/>}/>
-          <Route path="/cart"/>
+          <Route path="/cart" element={<CartPage/>}/>
           {/* <Route path="/dashboard" element={<DashboardPage/>}/> */}
           {/* <Route path="/verify" element={<VerifyPage/>}/> */}
           {/* <Route path="/" element={<h1>Home</h1>}/> */}
